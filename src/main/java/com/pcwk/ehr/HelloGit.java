e773712ee4c487d918e4a78f04054c5eefa932a9
김도형 <64787847+Dosn-code@users.noreply.github.com>
2022-01-18 14:47:52 +0900

    create project

---
+++ src/main/java/com/pcwk/ehr/HelloGit.java
@@ -0,0 +1,15 @@
+package com.pcwk.ehr;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloGit {
+	final static Logger LOG = LogManager.getFormatterLogger(HelloGit.class);
+	public static void main(String[] args) {
+		LOG.debug("================");
+		LOG.debug("====HelloGit====");
+		LOG.debug("================");
+
+	}
+
+}
---
+++ src/main/java/com/pcwk/ehr/PullHelloGit.java
@@ -0,0 +1,7 @@
+package com.pcwk.ehr;
+
+public class PullHelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello,world!");
+	}
+}
