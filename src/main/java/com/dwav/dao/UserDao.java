2a72bf793e98505cdce317a047aa2de6c570fb6d
JSPark <jisu229@naver.com>
2022-01-19 21:09:47 +0900

    커밋

--- src/main/java/com/dwav/dao/UserDao.java
+++ src/main/java/com/dwav/dao/UserDao.java
@@ -1,4 +1,4 @@
-package com.dwav.daoImp;
+package com.dwav.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
--- src/main/java/com/dwav/daoImp/UserDaoImpl.java
+++ src/main/java/com/dwav/daoImp/UserDaoImpl.java
@@ -24,6 +24,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Repository;
 
+import com.dwav.dao.UserDao;
 import com.dwav.vo.SearchVO;
 import com.dwav.vo.UserVO;
 
