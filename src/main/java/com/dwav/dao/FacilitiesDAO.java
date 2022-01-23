c053c3f447fd4722be992cee8ae170b1b983cf1c
dhkim <physicskdh@gmail.com>
2022-01-23 21:13:14 +0900

    update

--- src/main/java/com/dwav/dao/FacilitiesDAO.java
+++ src/main/java/com/dwav/dao/FacilitiesDAO.java
@@ -7,6 +7,7 @@ import com.dwav.vo.FacilitiesVO;
 public interface FacilitiesDAO {
 //	CRUDs
 	public int addAmenity(FacilitiesVO amen);
+	//각종 욕실용품과 소모품을 일컫는 말입니다.
 	public int addAmenityList(List<FacilitiesVO> amenList);
 	public List<FacilitiesVO> getAmenityListByAccomID(int accom_id);
 	public int updateAmentiyList(int accom_id, List<FacilitiesVO> amenList);
